c089386b4e1382d61e03c63d1e3ba95e77958718
Jakub Kubacki <kubacki.jk@gmail.com>
2018-03-30 19:44:04 +0700

    Rename UpdateRepositoriesService to ProcessRepositories

---
+++ app/services/process_repositories.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ProcessRepositories < ApplicationService
+  def call
+    FetchRepositories.call
+      .bind(method(:process_repositories))
+  end
+
+  private
+
+  def process_repositories(repository_names)
+    repository_names.each { |repository_name| schedule_job(repository_name) }
+    Success(nil)
+  end
+
+  def schedule_job(repository_name)
+    CallServiceObjectJob.perform_later(ProcessRepository, repository_name)
+  end
+end
