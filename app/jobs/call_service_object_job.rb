d17e33b8e78b0f47332c3ecdae082da31705e5bd
Jakub Kubacki <kubacki.jk@gmail.com>
2018-03-30 21:59:23 +0700

    Pass String to sidekiq jobs

--- app/jobs/call_service_object_job.rb
+++ app/jobs/call_service_object_job.rb
@@ -2,6 +2,6 @@
 
 class CallServiceObjectJob < ApplicationJob
   def perform(service_object, *arguments)
-    service_object.call(*arguments)
+    service_object.constantize.call(*arguments)
   end
 end
--- app/services/fetch_and_process_repositories.rb
+++ app/services/fetch_and_process_repositories.rb
@@ -14,6 +14,6 @@ class FetchAndProcessRepositories < ApplicationService
   end
 
   def schedule_job(repository_data)
-    CallServiceObjectJob.perform_later(ProcessRepository, repository_data)
+    CallServiceObjectJob.perform_later("ProcessRepository", repository_data)
   end
 end
