a1e958ecba340540232e2515a58f680b71201a0e
Adam Majkowski <adam.majkowski@netguru.pl>
2018-03-30 11:00:03 +0200

    Rename fetching method

--- app/services/gem_info_service.rb
+++ app/services/gem_info_service.rb
@@ -1,8 +1,10 @@
 class GemInfoService
+
   include HTTParty
   base_uri "rubygems.org/api/v1/gems"
 
-  def self.fetch_gem_info(gem)
+  def self.call(gem)
     response = get("/#{gem}.json")
   end
+
 end
