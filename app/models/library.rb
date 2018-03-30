1678d60d5b71b4656b585748cbb178bc63b949f5
mpiask <maciej.piaskowski@netguru.pl>
2018-03-30 10:26:39 +0200

    Added validations

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,3 +1,5 @@
 class Library < ApplicationRecord
   has_many :dependencies
+
+  validates :name, presence: true
 end
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,3 +1,6 @@
 class Repository < ApplicationRecord
   has_many :dependencies
+
+  validates :name, presence: true
+  validates :url, presence: true
 end
