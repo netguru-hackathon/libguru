644b173bef8db2d387dcc9d4ec9e1c52271318eb
Michał Maćkowiak <michal.mackowiak@netguru.pl>
2018-03-30 16:38:17 +0200

    Fixed routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :libraries
+  resources :libraries, only: [:index, :show]
 
   root to: "libraries#index"
   get "/auth/:provider/callback" => "sessions#create"
