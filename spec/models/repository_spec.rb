b3c788093bb2ecb9c8e5afee0783d956c0d254ee
Adam Majkowski <adam.majkowski@netguru.pl>
2018-04-03 12:14:51 +0200

    Add repository tests

---
+++ spec/models/repository_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+RSpec.describe Repository, type: :model do
+  it { should have_db_column(:name).of_type(:string) }
+  it { should have_db_column(:url).of_type(:string) }
+
+  it { should have_many(:dependencies).dependent(:destroy) }
+  it { should have_many(:libraries).through(:dependencies) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:url) }
+end
