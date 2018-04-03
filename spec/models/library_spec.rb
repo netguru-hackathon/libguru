5734cdcc2e89d189fd4198fe67dcd8c3a8017fc7
Adam Majkowski <adam.majkowski@netguru.pl>
2018-04-03 12:11:50 +0200

    Add library model specs

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -1,13 +1,14 @@
-# # frozen_string_literal: true
+# frozen_string_literal: true
 
-# describe Library, type: :model do
-#   before(:each) { @user = FactoryBot.create(:user) }
-#   subject { @user }
+RSpec.describe Library, type: :model do
+  it { should have_db_column(:name).of_type(:string) }
+  it { should have_db_column(:url).of_type(:string) }
+  it { should have_db_column(:info).of_type(:text) }
+  it { should have_db_column(:dependencies_count).of_type(:integer) }
 
-#   it { should have_db_column(:name).of_type(:string) }
-#   it { should have_db_column(:provider).of_type(:string) }
-#   it { should have_db_column(:uid).of_type(:string) }
-#   it { should have_db_column(:role).of_type(:integer) }
+  it { should have_many(:dependencies).dependent(:destroy) }
+  it { should have_many(:repositories).through(:dependencies) }
 
-#   it { should define_enum_for(:role).with_values([:user, :vip, :admin]) }
-# end
+  it { should validate_presence_of(:name) }
+end
+
